c762438b451cb98a0d18c17455b6132e0baff6ef
REPIDUS <moondj123@naver.com>
2018-05-24 22:02:26 +0900

    no message

--- tutorial/tutorial/urls.py
+++ tutorial/tutorial/urls.py
@@ -15,6 +15,7 @@ Including another URLconf
 """
 from django.conf.urls import url
 from django.contrib import admin
+from django.urls import path
 from django.conf.urls import url, include
 from rest_framework import routers
 from myapp import views
@@ -25,7 +26,7 @@ router = routers.DefaultRouter()
 router.register(r'persons', views.PersonViewSet)
 
 urlpatterns = [
-    url(r'^admin/', admin.site.urls),
+    path('admin/', admin.site.urls),
     url(r'^', include(router.urls)),
     url(r'^api-auth/', include('rest_framework.urls', namespace='rest_framework'))
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
