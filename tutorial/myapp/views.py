87dad843df44090d17521d1a4359524a26ad9d09
REPIDUS <moondj123@naver.com>
2018-05-31 12:53:58 +0900

    minor changes

--- tutorial/myapp/views.py
+++ tutorial/myapp/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from rest_framework import viewsets
 from myapp.serializers import PersonSerializer
+from serializers import PersonSerializer
 from myapp.models import Person
 
 # Create your views here.
